8d0cf87e82f5946b40fc4dc14f7749162e01587f
Narsing <kommetan@gmail.com>
2016-07-07 15:30:50 +0530

    class-constructer-getpip-getsuit

--- Cards/src/Cards/Cards.java
+++ Cards/src/Cards/Cards.java
@@ -1,7 +1,26 @@
 package Cards;
 
 public class Cards {
-	private Cards(){
-		
+        int sequence;
+	private Cards(int a){
+		sequence=a;
+	}
+
+        String getsuit(){
+	int arr[]=new int[52];
+	for(int i=0;i<52;i++){
+   	arr[i]=i;
+	if(sequence<=12&&sequence>=0)
+        return "clubs";
+	if(sequence<=25&&sequence>=13)
+	return "Diamonds";
+	if(sequence<=38&&sequence>=26)
+	return "Hearts";
+	else
+ 	return "spades";
+	}
+
+        int pip(){
+	return sequence%13+1;
 	}
 }
